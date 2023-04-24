9646ce478bb2e30bf44c3556248efc3a382d63e3
Bryce Robinson <bryce_robinson37@yahoo.com>
2023-04-24 16:12:03 -0500

    Working version of create account page

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@
 
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, CreateAccountForm
 
 
 @app.route('/')
@@ -30,4 +30,10 @@ def login():
 
 @app.route('/createAccount')
 def createAccount():
-    return render_template('createAccount.html')
+   form = CreateAccountForm()
+   if form.validate_on_submit():
+      flash('CreateAccount requested for nameF {}, nameL {}, email {}, password {}'.format(
+               form.nameF.data, form.nameL.data, form.email.data,
+               form.password.data))
+      return redirect(url_for('home'))
+   return render_template('createAccount.html', form=form)
