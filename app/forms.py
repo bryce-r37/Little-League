7297d0ce88a9eb493ed90a1e3ac10c90f8884e07
CarsonBuntin <carson_buntin1@baylor.edu>
2023-04-24 14:50:55 -0500

    Added littleleauge.sql

--- app/forms.py
+++ app/forms.py
@@ -6,20 +6,22 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Regexp, Length
 
+
 class CreateAccountForm(FlaskForm):
-   nameF = StringField('First Name', validators=[DataRequired()])
-   nameL = StringField('Last Name', validators=[DataRequired()])
-   email = StringField('Email Address', validators=[DataRequired(), Email("Please enter a valid Email", False, True)])
-   password = PasswordField('Password', validators=[DataRequired(), 
-                                                   Regexp("(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{1,100}",
-                                                         0,
-                                                         "Field must contain at least 1 uppercase, 1 lowercase, and 1 digit. "), 
-                                                   Length(8, 32)])
-   passwordConf = PasswordField('Confirm Password', 
-                               validators=[DataRequired(), EqualTo('password')])
-   submit = SubmitField('Create Account')
+    nameF = StringField('First Name', validators=[DataRequired()])
+    nameL = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired(), Email("Please enter a valid Email", False, True)])
+    password = PasswordField('Password', validators=[DataRequired(),
+                                                     Regexp("(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{1,100}",
+                                                            0,
+                                                            "Field must contain at least 1 uppercase, 1 lowercase, and 1 digit. "),
+                                                     Length(8, 32)])
+    passwordConf = PasswordField('Confirm Password',
+                                 validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Create Account')
+
 
 class LoginForm(FlaskForm):
-   email = StringField('Email', validators=[DataRequired()])
-   password = PasswordField('Password', validators=[DataRequired()])
-   submit = SubmitField('Sign In')
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
--- app/routes.py
+++ app/routes.py
@@ -2,34 +2,32 @@
 # authors: Bryce Robinson
 # date: 4/18/23
 
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm, CreateAccountForm
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-   return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/home')
 def home():
-   return render_template('home.html', title='Home')
+    return render_template('home.html', title='Home')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-   form = LoginForm()
-   if form.validate_on_submit():
-      flash('Login requested for email {}'.format(
-               form.email.data))
-      return redirect(url_for('home'))
-   return render_template('login.html', form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for email {}'.format(
+            form.email.data))
+        return redirect(url_for('home'))
+    return render_template('login.html', form=form)
 
-@app.route('/createAccount', methods=['GET', 'POST'])
+
+@app.route('/createAccount')
 def createAccount():
-   form = CreateAccountForm()
-   if form.validate_on_submit():
-      flash('CreateAccount requested for nameF {}, nameL {}, email {}, password {}'.format(
-               form.nameF.data, form.nameL.data, form.email.data,
-               form.password.data))
-      return redirect(url_for('home'))
-   return render_template('createAccount.html', form=form)
+    return render_template('createAccount.html')
