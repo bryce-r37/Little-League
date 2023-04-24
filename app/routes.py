d8b4e002ddc37a6c248947b9b69f0be8a1c61622
Bryce Robinson <bryce_robinson37@yahoo.com>
2023-04-24 17:38:52 -0500

    Added html for team stat pages

--- app/routes.py
+++ app/routes.py
@@ -37,3 +37,11 @@ def createAccount():
                form.password.data))
       return redirect(url_for('home'))
    return render_template('createAccount.html', form=form)
+
+@app.route('/team/pitching')
+def pitching():
+   return render_template('pitching.html')
+
+@app.route('/team/batting')
+def batting():
+   return render_template('batting.html')
