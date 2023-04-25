5fc22ae704db4b706ff88842601c6fca19e02d28
CarsonBuntin <carson_buntin1@baylor.edu>
2023-04-24 21:13:22 -0500

    Finish create account and progress on login

--- app/routes.py
+++ app/routes.py
@@ -4,12 +4,15 @@
 
 from flask import render_template, flash, redirect, url_for
 from flask_login import current_user
+from pymysql import IntegrityError
+from werkzeug.security import generate_password_hash, check_password_hash
 
+import pymysql
 from app import app
 from app.forms import LoginForm, CreateAccountForm
 
-from sql import sql
-from sql.sql import ValidateLogin, CreateAccount
+con = pymysql.connect(host='localhost', user='root', password='Cars0n200!', db='littleleauge')
+cur = con.cursor()
 
 
 @app.route('/')
@@ -27,31 +30,41 @@ def home():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        results = ValidateLogin(form)
-        if results is None or len(results) == 0:
-            flash("invalid emil or password")
-        else:
+        sql = "SELECT password FROM user WHERE email = %s"
+
+        params = [form.email.data]
+        cur.execute(sql, params)
+        result = cur.fetchall()
+        print(result)
+        print(check_password_hash(result[0][0], form.password.data))
+
+        if check_password_hash(result[0][0], form.password.data) and result is not None:
+            con.commit()
+            con.close()
             return redirect(url_for('home'))
+        else:
+            message = "Incorrect password or email."
+            return render_template('login.html', form=form, message=message)
     return render_template('login.html', form=form)
 
 
 @app.route('/createAccount', methods=['GET', 'POST'])
 def createAccount():
-   form = CreateAccountForm()
-   if form.validate_on_submit():
-      if CreateAccount(form):
-         return redirect(url_for('home'))
-      else IntegrityError:
-         message = 'Email already exists. Please choose a different email.'
-         return render_template('createAccount.html', form=form, message=message)
-   return render_template('createAccount.html', form=form)
+    form = CreateAccountForm()
+    message = None
+    if form.validate_on_submit():
+        sql = "INSERT INTO user (email, name, password) VALUES (%s, %s, %s)"
+        passwordHash = generate_password_hash(form.password.data)
+        name = form.nameF.data + " " + form.nameL.data
 
+        params = [form.email.data, name, passwordHash]
+        try:
+            cur.execute(sql, params)
+            con.commit()
+            con.close()
+            return redirect(url_for('home'))
+        except IntegrityError:
+            message = 'Email already exists. Please choose a different email.'
+            return render_template('createAccount.html', form=form, message=message)
 
-@app.route('/team/pitching')
-def pitching():
-   return render_template('pitching.html')
-
-
-@app.route('/team/batting')
-def batting():
-   return render_template('batting.html')
+    return render_template('createAccount.html', form=form)
