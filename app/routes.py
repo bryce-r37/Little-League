4fe0cc38b041b6ade187adb37bdfabf4787d317c
Bryce Robinson <bryce_robinson37@yahoo.com>
2023-04-25 08:54:21 -0500

    Refactored final create account/login sql to separate functions, modified db connection to use dictionary.

--- app/routes.py
+++ app/routes.py
@@ -4,16 +4,12 @@
 
 from flask import render_template, flash, redirect, url_for
 from flask_login import current_user
-from pymysql import IntegrityError
-from werkzeug.security import generate_password_hash, check_password_hash
 
-import pymysql
 from app import app
 from app.forms import LoginForm, CreateAccountForm
 
-con = pymysql.connect(host='localhost', user='root', password='Cars0n200!', db='littleleauge')
-cur = con.cursor()
-
+from sql import sql
+from sql.sql import ValidateLogin, CreateAccount
 
 @app.route('/')
 @app.route('/index')
@@ -30,45 +26,31 @@ def home():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        sql = "SELECT password FROM user WHERE email = %s OR username = %s"
-
-        params = [form.email.data.lower(), form.email.data]
-        cur.execute(sql, params)
-        result = cur.fetchall()
-
-        if result:
-            if check_password_hash(result[0][0], form.password.data):
-                con.commit()
-                return redirect(url_for('home'))
-            else:
-                message = "Incorrect password or Username/Email"
-                return render_template('login.html', form=form, message=message)
+        message = ValidateLogin(form)
+        if message == "Success":
+            return redirect(url_for('home'))
         else:
-            message = "Email/Username not found."
-            return render_template('login.html', form=form, message=message)
+           return render_template('login.html', form=form, message=message)
     return render_template('login.html', form=form)
 
 
 @app.route('/createAccount', methods=['GET', 'POST'])
 def createAccount():
-    form = CreateAccountForm()
-    message = None
-    if form.validate_on_submit():
-        sql = "INSERT INTO user (email, username, name, password) VALUES (%s, %s, %s, %s)"
-        passwordHash = generate_password_hash(form.password.data)
-        name = form.nameF.data + " " + form.nameL.data
+   form = CreateAccountForm()
+   if form.validate_on_submit():
+      message = CreateAccount(form)
+      if message == "Success":
+         return redirect(url_for('home'))
+      else:
+         return render_template('createAccount.html', form=form, message=message)
+   return render_template('createAccount.html', form=form)
 
-        params = [form.email.data.lower(), form.username.data, name, passwordHash]
-        try:
-            cur.execute(sql, params)
-            con.commit()
-            return redirect(url_for('home'))
-        except IntegrityError as e:
-            if 'PRIMARY' in str(e):
-                message1 = 'Email already exists. Please choose a different email.'
-                return render_template('createAccount.html', form=form, message1=message1)
-            else:
-                message2 = 'Username already exists. Please choose a different username.'
-                return render_template('createAccount.html', form=form, message2=message2)
 
-    return render_template('createAccount.html', form=form)
+@app.route('/team/pitching')
+def pitching():
+   return render_template('pitching.html')
+
+
+@app.route('/team/batting')
+def batting():
+   return render_template('batting.html')
--- sql/sql.py
+++ sql/sql.py
@@ -4,31 +4,46 @@
 
 import pymysql
 from pymysql import IntegrityError
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+from csi3335sp2023 import mysql
 
-con = pymysql.connect(host='localhost', user='root', password='Cars0n200!', db='littleleauge')
+con = pymysql.connect(host=mysql['location'], 
+                      user=mysql['user'], 
+                      password=mysql['password'], 
+                      database=mysql['database'])
 cur = con.cursor()
 
 def ValidateLogin(form):
-   sql = "SELECT * FROM user WHERE email = %s AND password = %s"
-   passwordHash = generate_password_hash(form.password.data)
+   sql = "SELECT password FROM user WHERE email = %s OR username = %s"
 
-   params = [form.email.data, passwordHash]
+   params = [form.email.data.lower(), form.email.data]
    cur.execute(sql, params)
 
-   return cur.fetchAll()
+   result = cur.fetchall()
+
+   if result:
+      if check_password_hash(result[0][0], form.password.data):
+         con.commit()
+         return "Success"
+      else:
+         return "Incorrect password."
+   else:
+      return "Email/Username not found."
 
 def CreateAccount(form):
-   sql = "INSERT INTO user (email, name, password) VALUES (%s, %s, %s)"
-        passwordHash = generate_password_hash(form.password.data)
-        name = form.nameF.data + " " + form.nameL.data
+   sql = "INSERT INTO user (email, username, name, password) VALUES (%s, %s, %s, %s)"
+   passwordHash = generate_password_hash(form.password.data)
+   name = form.nameF.data + " " + form.nameL.data
 
-        params = [form.email.data, name, passwordHash]
+   params = [form.email.data.lower(), form.username.data, name, passwordHash]
 
-        try:
-            cur.execute(sql, params)
-            con.commit()
-            con.close()
-            return True
-        except IntegrityError:
-            return False
+   try:
+      cur.execute(sql, params)
+      con.commit()
+      return "Success"
+   except IntegrityError as e:
+      if 'PRIMARY' in str(e):
+         return 'Email already exists. Please choose a different email.'
+      else:
+         return 'Username already exists. Please choose a different username.'
+
