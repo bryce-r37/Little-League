842ff74567c206e5572f160a4d6f98240552596e
Bryce Robinson <bryce_robinson37@yahoo.com>
2023-05-01 18:50:05 -0500

    Added images for website background, handling for selecting correct background based on user info

--- app/models.py
+++ app/models.py
@@ -1,11 +1,15 @@
 from flask_login import UserMixin
 from app import login
+from sql import sql
+from sql.sql import FetchUser
 
 class User(UserMixin):
-    def __init__(self, username):
+    def __init__(self, username, team):
         self.username=username
+        self.team=team
         self.id=username
 
 @login.user_loader
 def load_user(id):
-    return User(id)
+    user = FetchUser(id)
+    return User(user[0], user[1])
